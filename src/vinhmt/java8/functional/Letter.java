dbce823285526070fcacd76f1572d805a19eaef3
vinhmt1988 <vinhmt1988@gmail.com>
2017-08-01 02:14:51 +0700

    Add some stream examples

---
+++ src/vinhmt/java8/functional/Letter.java
@@ -0,0 +1,19 @@
+package vinhmt.java8.functional;
+
+public final class Letter
+{
+	public void addHeader(String header)
+	{
+
+	}
+
+	public void checkSpelling(String content)
+	{
+
+	}
+
+	public void addFooter(String footer)
+	{
+
+	}
+}
